bb76f241080546bf8c7ba35da684f3b2b3bd37c1
Vishal Kumar <25267239+vishalx360@users.noreply.github.com>
2020-10-11 12:57:14 +0530

    add attachment_moderator

--- src/bot.js
+++ src/bot.js
@@ -4,29 +4,38 @@ if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
 const { Client } = require("discord.js");
-
+// local modules
+const attachment_moderator = require("./moderators/attachment_moderator");
+// new Client instance
+const client = new Client();
 // bot token from env variable
 const BOT_TOKEN = process.env.BOT_TOKEN;
 
-// new Client instance
-const client = new Client();
+// load ml model
+attachment_moderator.load_model().then(() => {
+  client.login(BOT_TOKEN);
+});
 
-client.login(BOT_TOKEN);
 // ready event
 client.on("ready", () => {
   console.log(`${client.user.tag} Started`);
 });
 
 // message event
-client.on("message", (messageRef) => {
+client.on("message", async (messageRef) => {
   // ignore messages sent by a bot.
   if (messageRef.author.bot) return;
 
-  // TODO: MODERATION---
   // TODO: COMMANDS ---
+  // MODERATION---
+  // attachment moderation
+  messageRef.attachments.forEach(async (attachment) => {
+    let prediction = await attachment_moderator.moderate(attachment);
+    messageRef.reply(prediction);
+  });
 
   // TEST: set-up ---
-  if (messageRef.content === "hello") {
-    messageRef.reply("Hello");
-  }
+  // if (messageRef.content === "hello") {
+  //   response = "Hello";
+  // }
 });
