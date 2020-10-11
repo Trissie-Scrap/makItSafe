6068bfeb77c93dd26bbf449fe27159dbd25cbda0
Vishal Kumar <25267239+vishalx360@users.noreply.github.com>
2020-10-11 09:58:30 +0530

    add set-up test.

--- src/bot.js
+++ src/bot.js
@@ -16,3 +16,17 @@ client.login(BOT_TOKEN);
 client.on("ready", () => {
   console.log(`${client.user.tag} Started`);
 });
+
+// message event
+client.on("message", (messageRef) => {
+  // ignore messages sent by a bot.
+  if (messageRef.author.bot) return;
+
+  // TODO: MODERATION---
+  // TODO: COMMANDS ---
+
+  // TEST: set-up ---
+  if (messageRef.content === "hello") {
+    messageRef.reply("Hello");
+  }
+});
